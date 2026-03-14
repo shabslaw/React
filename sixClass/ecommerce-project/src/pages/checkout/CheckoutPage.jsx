8fb56e6bb10103240ec91ab7eabe092937f28a84
shabslaw <shabslaw@gmail.com>
2026-03-14 15:40:47 -0700

    created checkout componet and  group all related checkout files

--- sixClass/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ sixClass/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router";
-import "./checkout-header.css";
+import CheckoutHeader from "./CheckoutHeader";
 import "./CheckoutPage.css";
 
 function CheckoutPage() {
@@ -7,25 +6,7 @@ function CheckoutPage() {
         <>
             <title>Checkout</title>
 
-            <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                        <Link to="/">
-                            <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </Link>
-                    </div>
-
-                    <div className="checkout-header-middle-section">
-                        Checkout (<Link className="return-to-home-link"
-                            to="/">3 items</Link>)
-                    </div>
-
-                    <div className="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <CheckoutHeader />
 
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
