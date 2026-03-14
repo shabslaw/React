5bfbfac7265e42bd7921b19f3030ad1fc0484c21
shabslaw <shabslaw@gmail.com>
2026-03-14 13:27:02 -0700

    Split Header to Componet and change <a> element to <Link>

--- sixClass/ecommerce-project/src/pages/HomePage.jsx
+++ sixClass/ecommerce-project/src/pages/HomePage.jsx
@@ -1,68 +1,39 @@
-import "./header.css"
+import Header from "../components/Header";
 import "./HomePage.css"
+
 
 function HomePage(){
     return(
         <>
             <title>Ecommerce Project</title>
 
-            <div class="header">
-                <div class="left-section">
-                    <a href="/" class="header-link">
-                    <img class="logo"
-                        src="images/logo-white.png" />
-                    <img class="mobile-logo"
-                        src="images/mobile-logo-white.png" />
-                    </a>
-                </div>
+            <Header />
 
-                <div class="middle-section">
-                    <input class="search-bar" type="text" placeholder="Search" />
-
-                    <button class="search-button">
-                    <img class="search-icon" src="images/icons/search-icon.png" />
-                    </button>
-                </div>
-
-                <div class="right-section">
-                    <a class="orders-link header-link" href="/orders">
-
-                    <span class="orders-text">Orders</span>
-                    </a>
-
-                    <a class="cart-link header-link" href="/checkout">
-                    <img class="cart-icon" src="images/icons/cart-icon.png" />
-                    <div class="cart-quantity">3</div>
-                    <div class="cart-text">Cart</div>
-                    </a>
-                </div>
-            </div>
-
-            <div class="home-page">
-                <div class="products-grid">
-                    <div class="product-container">
-                    <div class="product-image-container">
-                        <img class="product-image"
+            <div className="home-page">
+                <div className="products-grid">
+                    <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image"
                         src="images/products/athletic-cotton-socks-6-pairs.jpg" />
                     </div>
 
-                    <div class="product-name limit-text-to-2-lines">
+                    <div className="product-name limit-text-to-2-lines">
                         Black and Gray Athletic Cotton Socks - 6 Pairs
                     </div>
 
-                    <div class="product-rating-container">
-                        <img class="product-rating-stars"
+                    <div className="product-rating-container">
+                        <img className="product-rating-stars"
                         src="images/ratings/rating-45.png" />
-                        <div class="product-rating-count link-primary">
+                        <div className="product-rating-count link-primary">
                         87
                         </div>
                     </div>
 
-                    <div class="product-price">
+                    <div className="product-price">
                         $10.90
                     </div>
 
-                    <div class="product-quantity-container">
+                    <div className="product-quantity-container">
                         <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -77,41 +48,41 @@ function HomePage(){
                         </select>
                     </div>
 
-                    <div class="product-spacer"></div>
+                    <div className="product-spacer"></div>
 
-                    <div class="added-to-cart">
+                    <div className="added-to-cart">
                         <img src="images/icons/checkmark.png" />
                         Added
                     </div>
 
-                    <button class="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary">
                         Add to Cart
                     </button>
                     </div>
 
-                    <div class="product-container">
-                    <div class="product-image-container">
-                        <img class="product-image"
+                    <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image"
                         src="images/products/intermediate-composite-basketball.jpg" />
                     </div>
 
-                    <div class="product-name limit-text-to-2-lines">
+                    <div className="product-name limit-text-to-2-lines">
                         Intermediate Size Basketball
                     </div>
 
-                    <div class="product-rating-container">
-                        <img class="product-rating-stars"
+                    <div className="product-rating-container">
+                        <img className="product-rating-stars"
                         src="images/ratings/rating-40.png" />
-                        <div class="product-rating-count link-primary">
+                        <div className="product-rating-count link-primary">
                         127
                         </div>
                     </div>
 
-                    <div class="product-price">
+                    <div className="product-price">
                         $20.95
                     </div>
 
-                    <div class="product-quantity-container">
+                    <div className="product-quantity-container">
                         <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -126,41 +97,41 @@ function HomePage(){
                         </select>
                     </div>
 
-                    <div class="product-spacer"></div>
+                    <div className="product-spacer"></div>
 
-                    <div class="added-to-cart">
+                    <div className="added-to-cart">
                         <img src="images/icons/checkmark.png" />
                         Added
                     </div>
 
-                    <button class="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary">
                         Add to Cart
                     </button>
                     </div>
 
-                    <div class="product-container">
-                    <div class="product-image-container">
-                        <img class="product-image"
+                    <div className="product-container">
+                    <div className="product-image-container">
+                        <img className="product-image"
                         src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
                     </div>
 
-                    <div class="product-name limit-text-to-2-lines">
+                    <div className="product-name limit-text-to-2-lines">
                         Adults Plain Cotton T-Shirt - 2 Pack
                     </div>
 
-                    <div class="product-rating-container">
-                        <img class="product-rating-stars"
+                    <div className="product-rating-container">
+                        <img className="product-rating-stars"
                         src="images/ratings/rating-45.png" />
-                        <div class="product-rating-count link-primary">
+                        <div className="product-rating-count link-primary">
                         56
                         </div>
                     </div>
 
-                    <div class="product-price">
+                    <div className="product-price">
                         $7.99
                     </div>
 
-                    <div class="product-quantity-container">
+                    <div className="product-quantity-container">
                         <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -175,14 +146,14 @@ function HomePage(){
                         </select>
                     </div>
 
-                    <div class="product-spacer"></div>
+                    <div className="product-spacer"></div>
 
-                    <div class="added-to-cart">
+                    <div className="added-to-cart">
                         <img src="images/icons/checkmark.png" />
                         Added
                     </div>
 
-                    <button class="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary">
                         Add to Cart
                     </button>
                     </div>
