3078c8627d4be268609ba03a5919c209011eaea8
shabslaw <shabslaw@gmail.com>
2026-03-17 13:23:33 -0700

    convert promise fetch Data to async & await

--- sixClass/ecommerce-project/src/App.jsx
+++ sixClass/ecommerce-project/src/App.jsx
@@ -12,10 +12,12 @@ function App() {
   const [cart, setCart] = useState([]);
   
   useEffect(()=>{
-    axios.get('/api/cart-items?expand=product')
-        .then((response)=>{
-            setCart(response.data);
-        });
+    const getAppData = async ()=>{
+      const response = await axios.get('/api/cart-items?expand=product')
+      setCart(response.data);
+    }
+
+    getAppData()
   }, []);
 
   return (
