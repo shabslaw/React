7d570eeee2cb970c0700963dfb73876219405abf
shabslaw <shabslaw@gmail.com>
2026-03-20 07:04:04 -0700

    Create Integration Test with Product.jsx component

--- sixClass/ecommerce-project/src/pages/home/Product.jsx
+++ sixClass/ecommerce-project/src/pages/home/Product.jsx
@@ -33,8 +33,11 @@ export function Product({ product, loadCartData }) {
     return (
         <div className="product-container">
             <div className="product-image-container">
-                <img className="product-image"
-                    src={product.image} />
+                <img 
+                    className="product-image"
+                    data-testid="product-image"
+                    src={product.image} 
+                />
             </div>
 
             <div className="product-name limit-text-to-2-lines">
@@ -42,8 +45,11 @@ export function Product({ product, loadCartData }) {
             </div>
 
             <div className="product-rating-container">
-                <img className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
+                <img 
+                    className="product-rating-stars"
+                    data-testid="product-rating-stars-image"
+                    src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                />
                 <div className="product-rating-count link-primary">
                     {product.rating.count}
                 </div>
